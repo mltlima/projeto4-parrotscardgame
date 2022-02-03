260dc274e999551c690eed09a961b5875c544116
mltlima <miguellt@dcc.ufrj.br>
2022-02-02 23:29:36 -0300

    distruicao aleatoria de cartas viradas para baixo

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
 let numCards = 0;
+const deck = [
+    "assets/bobrossparrot.gif",
+    "assets/explodyparrot.gif",
+    "assets/fiestaparrot.gif",
+    "assets/metalparrot.gif",
+    "assets/revertitparrot.gif",
+    "assets/tripletsparrot.gif",
+    "assets/unicornparrot.gif"
+]
 
-//Validates number inputted by user
-while (numCards < 4 || numCards > 14 || numCards % 2 !== 0) {
-    numCards = prompt("insira número par de cartas de 4 a 14");
+function shuffle() {
+    deck.sort(comparador);
+}
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+function generateDeck() {
+
+    const main = document.querySelector("main");
+    shuffle()
+
+    for (let i = 0; i < numCards ; i++) {
+        main.innerHTML += `<div class="card">
+                                <div class="front-card">
+                                    <img src="assets/front.png">
+                                </div>
+                                <div class="back-card hide">
+                                <img src="${deck[i]}">
+                                </div>
+                            </div>
+                        `
+    }
+}
+
+function startGame() {
+    
+    //Validates number inputted by user
+    while (numCards < 4 || numCards > 14 || numCards % 2 !== 0) {
+        numCards = prompt("insira número par de cartas de 4 a 14");
+    }
+
+    generateDeck();
 }
